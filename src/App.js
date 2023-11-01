87d755b4e7bfc76b8590b2d69f827154b92504e7
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-11-01 18:21:57 +0100

    Did the setup of my dictionary project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Dictionary from './Dictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Dictionary/>
+      <footer>
+        This project was coded by Ebba Emanuelsson and is{" "}
+        <a
+          className="App-link"
+          href=" "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourecd on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
