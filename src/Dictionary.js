82bfee91bb66bef034953860661b90be0bd78bb7
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-11-01 23:45:57 +0100

    made the search form work

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 
 export default function Dictionary(){
-    return(
-        <div>
-            Dictionary
-        </div>
-    )
+    let [keyword, setKeyword] = useState("");
+
+    function search(event){
+        event.preventDefault();
+        alert(`${keyword}`);
+    }
+
+    function handleKeywordChange(event){
+         setKeyword(event.target.value);
+    }
+
+    return (
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" onChange={handleKeywordChange}/>
+        </form>
+    </div>
+    );
 }
