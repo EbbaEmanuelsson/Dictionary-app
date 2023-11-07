af4844e8b0045a8a5b9d2860e386a3feb4963841
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-11-07 15:02:01 +0100

    x

--- src/Photos.js
+++ src/Photos.js
@@ -17,6 +17,7 @@ export default function Photos(props){
                               <img
                                 src={photo.src.landscape}
                                 className="img-fluid"
+                                alt="dictionary"
                               />
                             </a>
                           </div>
